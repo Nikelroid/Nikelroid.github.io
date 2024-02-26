6b89ec15a0badd9de6ceff049c8b6f4038a48bf3
Nikelroid <Nikelroid@gmail.com>
2024-02-26 15:48:28 +0330

    correct navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -34,7 +34,7 @@
                         <router-link class="block py-2 pl-3 pr-4 hover:text-link-color" to="/#about-section">About</router-link>
                     </li>
                     <li :class="['transition-all motion-reduce:transition-none duration-500 delay-[100ms]', showTransition ? 'translate-y-0 opacity-1' : '-translate-y-4 opacity-0']">
-                        <router-link class="block py-2 pl-3 pr-4 hover:text-link-color" to="/#experience-section">Experience</router-link>
+                        <router-link class="block py-2 pl-3 pr-4 hover:text-link-color" to="/#experience-section">Education</router-link>
                     </li>
                     <li :class="['transition-all motion-reduce:transition-none duration-500 delay-[150ms]', showTransition ? 'translate-y-0 opacity-1' : '-translate-y-4 opacity-0']">
                         <router-link class="block py-2 pl-3 pr-4 hover:text-link-color" to="/#work-section">Projects</router-link>
